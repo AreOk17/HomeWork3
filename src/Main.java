e772eab420c5d5deefd36b6bd65bff8e603478e8
1 <Anasko162@Mail.ru>
2022-11-05 02:18:01 +0300

    ДЗ С else. Не получается обьявить двойное условие.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        boolean ChildVozrast = true ;
         int Age = 16;
         System.out.println("Возраст ребенка равен  " +Age);
         if (Age =>2 || Age <= 6) {
             System.out.println("Вам нужно ходить в десткий сад, вам: " + Age + " Лет");
         } else {
             System.out.println("Вам нужно в школу, вам : " + Age + " Леет");
-            }
+        }
     }
-    }
+}

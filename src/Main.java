7ef352c4a4a6102dc8181617213683ac563f9ce5
1 <Anasko162@Mail.ru>
2022-11-05 00:13:50 +0300

    ДЗ.

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int ages = 8;
+        if (ages>=7) {
+            System.out.println("Ребенок ходит в школу ");
+            int AgesHuman = 100;
+            if (AgesHuman>=18) {
+                System.out.println("Вы уже закончили школу и можете отправляться в университет. ");
+                int VzrosliyChelovek = 25;
+                if (VzrosliyChelovek>=24) {
+                    System.out.println("Вы окончили университет и пора искать первую работу. ");
+                    int StandingPassagere = 43;
+                    int SteatedPassagere = 63;
+                            if (SteatedPassagere>=60) {
+                                System.out.println("Cидячих мест нет ");
+                                if (StandingPassagere>=42); {
+                                System.out.println("Стоячих мест нет ");
+
+
+
+                            }
+
+                            }
+
+                }
+
+            }
+
+        }
+    }
